490824e4cc9e12c93543aabd1d07b595dc886b79
Yucheng7713 <yuchengh@usc.edu>
2018-10-15 13:55:43 -0700

    Integrate FBSDK and login feature. [PECO-129]

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,11 +1,20 @@
 // Required components from React, React Navigation, and Native Base
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, ImageBackground } from 'react-native';
-import { Button, Container, Content, Form, Item as FormItem, Input, Title } from 'native-base'
+import { Platform, StyleSheet, Text, View, ImageBackground, Alert } from 'react-native';
+import { Button, Container, Content, Form, Item as FormItem, Input, Title } from 'native-base';
+import { LoginManager, LoginButton, AccessToken, GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
 import { onSignIn } from "../src/auth";
 
 export default class SignIn extends Component<Props> {
   // Layout rendering : note that do not include any comment in return(...), it will be interpreted as layout component
+  state = {
+    userid: 'NEED_TO_BE_PROVIDED',
+    name: 'Perfecto User',
+    password: 'NEED_TO_BE_PROVIDED',
+    email: 'perfectodemo@email.com',
+    profile_url: "../assets/Profile/default-profile.png"
+  }
+
   render() {
     return (
       <Container>
@@ -16,22 +25,89 @@ export default class SignIn extends Component<Props> {
           <Title style={ styles.welcomeBackStyle}> Welcome Back ! </Title>
           <Form style={ styles.inputField }>
             <FormItem>
-              <Input placeholder="Username" placeholderTextColor="#808080" />
+              <Input placeholder="Username"
+              placeholderTextColor="#808080"
+              onChangeText={(textValue) => this.state.userid = textValue}/>
             </FormItem>
             <FormItem>
-              <Input placeholder="Password" placeholderTextColor="#808080" secureTextEntry={true} />
+              <Input placeholder="Password"
+              placeholderTextColor="#808080"
+              onChangeText={(textValue) => this.state.password = textValue}
+              secureTextEntry={true} />
             </FormItem>
           </Form>
           <View style={ styles.loginButtonLayout }>
             <Button style={ styles.loginButtonStyle }
-            onPress={() => { onSignIn().then(() => this.props.navigation.navigate("SignedIn")); }}>
+            onPress={() => {
+                if (onSignIn(this.state.userid, this.state.password)) {
+                  this.props.navigation.navigate("SignedIn");
+                } else {
+                  Alert.alert(
+                    'Invalid login information',
+                    'Username / Password unmatched',
+                    [
+                      {text: 'OK', onPress: () => console.log('OK Pressed')}
+                    ],
+                    { cancelable: false }
+                  )
+                }
+              }
+            }>
               <Text style={{ color: 'white' }}>Login</Text>
             </Button>
+          </View>
+          <View style={ styles.loginButtonLayout }>
+            <LoginButton
+            readPermissions={["email", "user_friends", "public_profile"]}
+            onLoginFinished={
+              (error, result) => {
+                if (error) {
+                  console.log("login has error: " + result.error);
+                } else if (result.isCancelled) {
+                  console.log("login is cancelled.");
+                } else {
+                  AccessToken.getCurrentAccessToken().then(
+                    (data) => {
+                      const infoRequest = new GraphRequest('/me', {
+                        accessToken: data.accessToken,
+                        parameters: {
+                            fields: {
+                               string: 'email,name,first_name,last_name,picture'
+                            }
+                        }
+                      }, this._responseInfoCallback);
+                      // Start the graph request.
+                      new GraphRequestManager().addRequest(infoRequest).start();
+                    }
+                  )
+                }
+              }
+            }
+            onLogoutFinished={() => console.log("logout.")}/>
           </View>
         </Content>
         </ImageBackground>
       </Container>
     );
+  }
+
+  //Create response callback.
+  _responseInfoCallback = (error, result) => {
+    if (error) {
+      console.log('Error fetching data: ' + error.toString());
+    } else {
+      // Fetch user Facebook info - place static data as test
+      this.setState({
+        userid: 'yucheng8168',
+        name: result.name,
+        password: 'steven0824',
+        email: result.email,
+        profile_url: result.picture.data.url
+      });
+      if (onSignIn(this.state.userid, this.state.password)) {
+        this.props.navigation.navigate("SignedIn");
+      }
+    }
   }
 }
 
